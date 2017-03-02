dc352c5b52f75002a8ce131dec266ecf47917f04
Alleo Indong <freedom_war550@live.com.ph>
2017-03-03 01:10:01 +0800

    added example drawer and progressbar

--- src/screens.js
+++ src/screens.js
@@ -1,6 +1,6 @@
 import { Navigation } from 'react-native-navigation';
 
-import Login from './modules/Login/';
+import Login from './modules/login/Login';
 
 export function registerScreens(store, Provider) {
     Navigation.registerComponent('Login', () => Login, store, Provider);
