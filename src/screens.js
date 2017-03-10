12a221d422e47ab33de7a7120d17c747b3af7dff
Alleo Indong <freedom_war550@live.com.ph>
2017-03-10 18:09:25 +0800

    going back to react-navigation since react-native-navigation is unstable on android

--- src/screens.js
+++
@@ -1,7 +0,0 @@
-import { Navigation } from 'react-native-navigation';
-
-import Login from './modules/login/Login';
-
-export function registerScreens(store, Provider) {
-    Navigation.registerComponent('Login', () => Login, store, Provider);
-}
